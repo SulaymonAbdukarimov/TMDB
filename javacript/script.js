69b6b6aebd0d20928a180fa4ed32365d3e98db8c
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2021-11-23 15:37:09 +0500

    scroll added

--- javacript/script.js
+++ javacript/script.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY; //WHAT IS POPULARS API
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const searchURL = BASE_URL + "/search/movie?" + API_KEY;
-const main = document.getElementById("main");
+const main = document.getElementById("movies-list");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const search_btn = document.getElementById("search-button");
